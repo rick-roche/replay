83e4f9ce133d2f056de40c84c965ed2b6c0c0f52
Richard Roché <richard.roche@investec.com>
2026-01-06 17:16:54 +0000

    feat: add external registry and azure container apps

--- src/RePlay.AppHost/AppHost.cs
+++ src/RePlay.AppHost/AppHost.cs
@@ -1,4 +1,11 @@
+#pragma warning disable ASPIRECOMPUTE003
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var endpoint = builder.AddParameter("registry-endpoint");
+var repository = builder.AddParameter("registry-repository");
+
+var registry = builder.AddContainerRegistry("container-registry", endpoint, repository);
 
 var server = builder.AddProject<Projects.RePlay_Server>("server")
     .WithHttpHealthCheck("/health")
